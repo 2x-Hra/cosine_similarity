8186bd7a3734d90b993ffa5762eec46927c19a2a
Hossein2x <hrakbari77@gmail.com>
2020-05-29 15:05:15 +0430

    reading file in rows added

--- cosine_similarity.py
+++ cosine_similarity.py
@@ -6,3 +6,17 @@ import math
 from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
+
+def rows_readFile(file_name):
+    '''
+        this function will read the file row by row
+    '''
+    
+    csv_arr =[]
+    with open(file_name, 'r') as csv_file:
+        csv_reader = reader(csv_file)
+        for row in csv_reader:
+            csv_arr.append(row)
+        
+    
+    return csv_arr
